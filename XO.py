56aa92a827727dc5d02e1d7762147d4a41664b6b
Amir <amir.foroughi85@gmail.com>
2023-08-05 14:24:34 +0330

    Game done

--- XO.py
+++ XO.py
@@ -16,7 +16,11 @@ def print_table(A):
 def put(a, n):
     row = (n - 1) // 3
     column = (n - 1) % 3
-    A[row][column] = a
+    if A[row][column] != "X" and A[row][column] != "O":
+        A[row][column] = a
+    else:
+        print(f"{y} YOU CHEATED!!!")
+        exit(0)
 
 def check(A):
     for i in range(3):
@@ -32,3 +36,31 @@ def check(A):
     return False
 
 A = [[0] * 3 for i in range(3)]
+fill(A)
+print("In each turn put the number of an empty square to fill it with your symbol.")
+player1 = input("Name of the first player is: ")
+player2 = input("Name of the second player is: ")
+input("Press enter when you are ready...")
+print()
+print_table(A)
+print()
+for i in range(9):
+    print(f"{player1} It's your turn.")
+    y = player1
+    s = int(input())
+    put("X", s)
+    print_table(A)
+    print()
+    if check(A):
+        print(f"{y} you WON!")
+        break
+
+    print(f"{player2} It's your turn.")
+    y = player2
+    s = int(input())
+    put("O", s)
+    print_table(A)
+    print()
+    if check(A):
+        print(f"{y} you WON!")
+        break
